957c6850195a3547b2d4e4ab82851196f7e87552
Cyrus Maher <cmaher@vir.bio>
2022-01-05 14:28:14 -0800

    Remove comments that broke run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -5,11 +5,10 @@ DATA=${HOME}/Data # Update to the directory where you want to store the data
 OUT_DIR="forecast" # the name of the output directory within $DATA
 METADATA_FNAME="metadata_oct2021.tsv"  # name of your GISAID metadata file
 
-
 docker run -it \
--v ${CODE}:/root/Code \  # mount the code directory
--v ${DATA}:/root/Data \  # mount the data directory
-${CONTAINER_ID} \  # specify the container ID
+-v ${CODE}:/root/Code \
+-v ${DATA}:/root/Data \
+${CONTAINER_ID} \
 bash -c "conda run -n sars2-forecasting python /root/Code/forecasting.py  \
-/root/Data/${METADATA_FNAME}  \
+/root/Data/${METADATA_FNAME} \
 /root/Data/${OUT_DIR}"
